8e5d496080c0a804c7cda6219e7be866aa20396f
debangana2298 <debanganabi@gmail.com>
2026-03-20 15:17:56 +0530

    code update

--- practice/src/main/java/practice/removeAllOccurance.java
+++ practice/src/main/java/practice/removeAllOccurance.java
@@ -1,9 +1,18 @@
-package practice;
+package practice.practice.src.main.java.practice;
 
 public class removeAllOccurance {
+	public static String removeOcc(String s, char ch) {
+		String results= " ";
+		String str= String.valueOf(ch);
+		return results= s.replace(str, "");
+		 
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String s= "banana";
+		char ch='a';
+		System.out.print(removeOcc(s,ch));
 
 	}
 
